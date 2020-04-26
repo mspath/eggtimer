1ecd35ad39f94b8db1a59e4ee18bdf01f76aeeef
Markus Spath <markus@hackr.de>
2020-04-26 08:20:02 +0200

    minor cleanups

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -24,13 +24,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.eggtimernotifications.R
 import com.example.android.eggtimernotifications.databinding.FragmentEggTimerBinding
-import com.google.firebase.messaging.FirebaseMessaging
 
 class EggTimerFragment : Fragment() {
 
@@ -45,12 +43,11 @@ class EggTimerFragment : Fragment() {
             inflater, R.layout.fragment_egg_timer, container, false
         )
 
-        val viewModel = ViewModelProviders.of(this).get(EggTimerViewModel::class.java)
+        val viewModel = ViewModelProvider(this).get(EggTimerViewModel::class.java)
 
         binding.eggTimerViewModel = viewModel
         binding.lifecycleOwner = this.viewLifecycleOwner
 
-        // TODO: Step 1.7 call create channel
         createChannel(
             getString(R.string.egg_notification_channel_id),
             getString(R.string.egg_notification_channel_name)
@@ -59,15 +56,12 @@ class EggTimerFragment : Fragment() {
     }
 
     private fun createChannel(channelId: String, channelName: String) {
-        // TODO: Step 1.6 START create a channel
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel = NotificationChannel(
                 channelId,
                 channelName,
-                // TODO: Step 2.4 change importance
                 NotificationManager.IMPORTANCE_HIGH
             )
-            // TODO: Step 2.6 disable badges for this channel
                 .apply {
                     setShowBadge(false)
                 }
@@ -80,12 +74,8 @@ class EggTimerFragment : Fragment() {
             val notificationManager = requireActivity().getSystemService(
                 NotificationManager::class.java
             )
-            notificationManager.createNotificationChannel(notificationChannel)
-
-
-            // TODO: Step 1.6 END create channel
+            notificationManager?.createNotificationChannel(notificationChannel)
         }
-
     }
 
     companion object {
--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
@@ -118,15 +118,7 @@ class EggTimerViewModel(private val app: Application) : AndroidViewModel(app) {
                     else ->timerLengthOptions[timerLengthSelection] * minute
                 }
                 val triggerTime = SystemClock.elapsedRealtime() + selectedInterval
-
-                // TODO: Step 1.5 get an instance of NotificationManager and call sendNotification
-//                val notificationManager = ContextCompat.getSystemService(
-//                    app,
-//                    NotificationManager::class.java
-//                ) as NotificationManager
-//                notificationManager.sendNotification(app.getString(R.string.timer_running), app)
-
-                // TODO: Step 1.15 call cancel notification
+                
                 val notificationManager =
                     ContextCompat.getSystemService(
                         app,
